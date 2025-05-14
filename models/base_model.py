4d2c60285ebb652c8af3a5a6883c968a30ecc3e1
aziza20-ux <s.afadhali@alustudent.com>
2025-05-14 13:17:40 +0200

    the updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
 import uuid
-form datetime import datetime
+from datetime import datetime
 
 class BaseModel:
     """the constructor of the class '__init__'"""
     def __init__(self, *args, **kwargvs):
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargvs:
+            for k, v in kwargvs.items():
+                self.__dict__[k] = v
 
     def __str__(self):
         """it will return the string represation of object"""
 
-        return f"[{self.__class__.__name__}] ({self.id} {self.__dict__})"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """this method will save the last time object was modified"""
@@ -24,14 +28,15 @@ class BaseModel:
         """the method to returm dictionary of the attributes of any object"""
         obj_dict = self.__dict__.copy()
 
-        obj_dict['__class__'] = self.__class__.name
+        obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         
         return obj_dict
 
 if __name__ == '__main__':
-    c = basemodel(city = kigali, house = 2rooms)
+    c = BaseModel(city="kigali", house="2rooms")
     c.save()
-    c.to_dict
-    c.__str__()
+    print(c.to_dict())
+    
+    
